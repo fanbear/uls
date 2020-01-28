d0b2a3acf53d96caa21ea16458a9a468c9b9e69b
tryznav <tryznav@MacBook-Air-6.local>
2020-01-28 15:05:14 +0200

    adding some code for merging

--- src/main.c
+++ src/main.c
@@ -12,10 +12,13 @@ int main(int argc, char **argv) {
 	t_args *args = mx_sort_args(argc, argv);
 	t_dirs *dirs = mx_get_dir_entry(args, mx_arr_size(args->dirs));
 	char symbol = '.';
+	bool symbol_l = false;
 	if (args->flags) {
 		for (int i = 0; args->flags[i]; i++) {
 			if (args->flags[i] == 'a')
 				symbol = '\0';
+			if (args->flags[i] == 'l')
+				symbol_l = true;	
 		}
 	}
 	if (args->dirs[0]) {
@@ -24,15 +27,23 @@ int main(int argc, char **argv) {
 				mx_print_ls_multy_colomn(args->files);
 				mx_printchar('\n');
 			}
-			mx_print(args, dirs, symbol, mx_print_ls_multy_colomn);
+			if (!symbol_l)
+				mx_print(args, dirs, symbol, mx_print_ls_multy_colomn);
+			else
+				mx_print(args, dirs, symbol, mx_print_ls_l);
 		} else {
 			if (args->files[0] != NULL) {
 				mx_print_ls_monocolomn(args->files);
 				mx_printchar('\n');
 			}
-			mx_print(args, dirs, symbol, mx_print_ls_monocolomn);
+			if (!symbol_l)
+				mx_print(args, dirs, symbol, mx_print_ls_monocolomn);
+			else
+				mx_print(args, dirs, symbol, mx_print_ls_l);
 		}
 	}
+
+
 	mx_del_dirs_struct(dirs);
 	mx_del_args_struct(args, NOT_VALID);
 	// system("leaks -q uls");
