ec160c6bf17f81bea4d001a89bef4f8ea100fb4b
fanbear <40995227+fanbear@users.noreply.github.com>
2020-02-06 18:53:30 +0200

    add file -l output (with bug)

--- src/mx_print_ls_l.c
+++ src/mx_print_ls_l.c
@@ -1,5 +1,6 @@
 #include "uls.h"
 
+static void print_files(t_args *args);
 static void print_dirs(t_dirs *dirs);
 
 static void print_space(int max_elem, char* str) {
@@ -17,13 +18,11 @@ static void print_total(int sum_total) {
 
 
 void mx_print_ls_l(t_args *args, t_dirs *dirs) {
-    // if (args->files[0]) {
-    //     mx_print(args->files);
-    //     if (dirs)
-    //         mx_printchar('\n');
-    // }
-	// printf("dirs = %d\n", mx_arr_size(args->dirs));
-	// exit(1);
+	if (args->files) {
+		print_files(args);
+		if (dirs)
+			mx_printchar('\n');
+	}
     if (dirs) {
         if (dirs->next || args->not_valid[0] || args->files[0])
             while (dirs) {
@@ -52,10 +51,8 @@ void mx_print_ls_l(t_args *args, t_dirs *dirs) {
 static void print_dirs(t_dirs *dirs) {
     t_dirs_entry *temp = dirs->entry_dir;
 
-
 	print_total(dirs->total);
     while (temp) {
-		// printf("dirs->max_time = %d, temp->stat->time = %d\n", dirs->max_time, mx_strlen(temp->stat->time));
         mx_printstr(temp->stat->permiss);
         mx_printchar(' ');
         print_space(dirs->max_link, temp->stat->nlink);
@@ -83,5 +80,42 @@ static void print_dirs(t_dirs *dirs) {
 		}
 		mx_printchar('\n');
         temp = temp->next;
+    }
+}
+
+static void print_files(t_args *args) {
+	int i = 0;
+
+	while (args->files[i]) {
+		t_file *stat = mx_get_stat(args->files[i]);
+
+		mx_printstr(stat->permiss);
+        mx_printchar(' ');
+        // print_space(max_link, stat->nlink);
+        mx_printstr(stat->nlink);
+        mx_printchar(' ');
+        mx_printstr(stat->user_name);
+        // print_space(max_user, stat->user_name);
+        mx_printchar(' ');
+        mx_printchar(' ');
+        mx_printstr(stat->group_name);
+        // print_space(max_group, stat->group_name);
+        mx_printchar(' ');
+        mx_printchar(' ');
+		// print_space(max_size, stat->file_size);
+        mx_printstr(stat->file_size);
+        mx_printchar(' ');
+        mx_printstr(stat->time1);
+		// print_space(max_time, stat->time2);
+		mx_printstr(stat->time2);
+        mx_printchar(' ');
+        mx_printstr(args->files[i]);
+		if (stat->name_link[0]) {
+			mx_printstr(" -> ");
+			mx_printstr(stat->name_link);
+		}
+		mx_printchar('\n');
+		i++;
+		
     }
 }
