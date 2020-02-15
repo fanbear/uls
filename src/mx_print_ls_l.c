b028da7e020d9faedebdc174d776cfdc0369bc62
fanbear <40995227+fanbear@users.noreply.github.com>
2020-02-15 10:19:03 +0200

    print_ls_l.c

--- src/mx_print_ls_l.c
+++ src/mx_print_ls_l.c
@@ -1,8 +1,7 @@
 #include "uls.h"
 
 static void print_dir(t_dirs *dirs) {
-	if (mx_check_on_access(1, dirs->dir));
-	else {
+	if (!mx_check_on_access(1, dirs->dir)) {
 		mx_printstr(dirs->dir);
 		mx_printstr(":\n");
 		mx_print_dirs_ls(dirs);
@@ -15,19 +14,13 @@ void mx_print_ls_l(t_args *args, t_dirs *dirs) {
 		if (dirs)
 			mx_printchar('\n');
 	}
-    if (dirs) {
-        if (dirs->next || args->not_valid[0] || args->files[0])
-            while (dirs) {
-				print_dir(dirs);
-				dirs = dirs->next;
-				if (dirs) {
-					mx_printchar('\n');
-				}
-            }
-        else {
-			if (!mx_check_on_access(0, dirs->dir)) {
-				mx_print_dirs_ls(dirs);
-			}
-		}
-    }
+    if (dirs->next || args->not_valid[0] || args->files[0])
+        while (dirs) {
+			print_dir(dirs);
+			dirs = dirs->next;
+			if (dirs)
+				mx_printchar('\n');
+        }
+    else if (!mx_check_on_access(0, dirs->dir))
+		mx_print_dirs_ls(dirs);
 }
